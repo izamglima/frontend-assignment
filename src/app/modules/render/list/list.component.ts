b7963ecb1518d2e8b12c10bb98ef40a25129d5d3
Izabela Lima <izamglima@gmail.com>
2020-10-14 15:09:08 +0200

    added cache in app service

--- src/app/modules/render/list/list.component.ts
+++ src/app/modules/render/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from './../../../services/app.service';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  objects;
   test = [{
     name: 'Circle',
     description: 'Rounded line'
@@ -18,9 +20,13 @@ export class ListComponent implements OnInit {
     name: 'Circle3',
     description: 'Rounded line'
   }];
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
+    this.appService.getObjects().subscribe((response: any) => {
+      this.objects = response;
+      console.log(this.objects);
+    });
   }
 
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { publishReplay, refCount, catchError } from 'rxjs/operators';
+
+type Objects = { text: string, urls: string };
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppService {
 
-  constructor() { }
+export class AppService {
+  objects: Observable<Objects>;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getObjects(): Observable<Objects> {
+    if (!this.objects) {
+      this.objects = this.httpClient.get<Objects>('assets/objects.json').pipe(
+        publishReplay(1),
+        refCount(),
+        catchError(this.handleError)
+      );
+    }
+
+    return this.objects;
+  }
+
+  handleError(error: HttpErrorResponse): Observable<any> {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Client side error occurred: ${error.error.message}`;
+    } else {
+      errorMessage = `Backend returned error code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
