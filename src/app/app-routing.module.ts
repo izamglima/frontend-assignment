6fdc6377e3fd7931375ceb324d608b889eb69030
Izabela Lima <izamglima@gmail.com>
2020-10-14 19:09:22 +0200

    making home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'list',
     loadChildren: () => import('./modules/render/render.module').then(m => m.RenderModule)
